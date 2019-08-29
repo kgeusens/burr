3c36bf4230536a71e4b5b65daaf4d77a13430388
kgeusens <koen_geusens@hotmail.com>
2019-08-30 00:44:38 +0200

    updated template to use Entity

--- src/components/BurrShape.vue
+++ src/components/BurrShape.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-      <div v-for="vox in voxelPositions" :key="vox.id">
-        <Box ref="myBoxes" :position="vox.position">
+      <Entity>
+        <Box v-for="vox in voxelPositions" :key="vox.id" ref="myBoxes" :position="vox.position">
         </Box>
-      </div>  
+      </Entity>  
   </div>
 </template>
 
 <script>
 
 import { rotationVector } from "./burrUtils.js";
-import { BABYLON, Box } from 'vue-babylonjs';
+import { BABYLON, Entity, Box } from 'vue-babylonjs';
 
 export default {
   name: 'BurrShape',
-  components: { Box },
+  components: { Box, Entity },
   props: 
   {
 //    entity: {_text: ["x"] }
@@ -23,63 +23,38 @@ export default {
   data() {
     return {
       id: null,
-//      myBoxes: [],
+      myBoxes: null,
       myEntity: null,
       theShapeMesh: null
     }
   },
   mounted () {
     this.id = this._uid
-    console.log("mounted", this.$refs.myBoxes)
   },
   beforeDestroy() {
     if (this.theShapeMesh) this.theShapeMesh.dispose()
   },
   updated() {
-    console.log("updated", this.$refs.myBoxes)
+    console.log("updated", this.$refs.myBoxes[0].$entity)
+//    this.reShape()
   },
   methods: {
     isVisible(dx,dy,dz) { return ( this.stateString[ (dz)*this.x*this.y + (dy)*this.x + (dx) ] == "#") },
     isEmpty(dx,dy,dz) { return ( this.stateString[ (dz)*this.x*this.y + (dy)*this.x + (dx) ] == "_") },
-    rotate(idx) { return rotationVector(idx); }
-  },
-  computed: {
-    myBoxes() {
-      return this.$refs.myBoxes
-    },
-    stateString() {
-      return this.entity._text[0].replace(/\d+/g,""); //remove color code
-    },
-    voxelPositions() {
-      var voxArr = new Array();
-      var theId = 0
-      for (let dx=0; dx < this.x; dx++) {
-        for (let dy=0; dy < this.y; dy++) {
-          for (let dz=0; dz < this.z; dz++) {
-            if (this.isVisible(dx, dy, dz)) { voxArr.push({ position: [dx, dy, dz], id:theId }); theId++ }
-          }
-        }
-      }
-      return voxArr
-    },
-    x() { return this.entity._attributes.x},
-    y() { return this.entity._attributes.y},
-    z() { return this.entity._attributes.z},
-    name() { return this.entity._attributes.name},
-    numOfBoxes() { return this.myBoxes.length}
-  },
-  watch: {
-    myBoxes() {
-/*      if (this.theShapeMesh)  { this.theShapeMesh.dispose()}
+    rotate(idx) { return rotationVector(idx); },
+    reShape() {
+//      debugger
+      this.myBoxes=this.$refs.myBoxes
+      if (this.theShapeMesh)  { this.theShapeMesh.dispose()}
       // the boxes that make up this shape have now been created.
       // We will merge them into one shape and and hide the individual boxes
-      var theCSG=BABYLON.CSG.FromMesh(this.myBoxes[0])
+      var theCSG=BABYLON.CSG.FromMesh(this.myBoxes[0].$entity)
       this.myBoxes.forEach(box => {
-        theCSG.unionInPlace(BABYLON.CSG.FromMesh(box))
+        theCSG.unionInPlace(BABYLON.CSG.FromMesh(box.$entity))
       })
-      // make the shape a little bis smaller. Start with the edges.
+      // make the shape a little bit smaller. Start with the edges.
       var bbox=BABYLON.Mesh.CreateBox("bbox",1)
-      bbox.parent=this.myBoxes[0].parent
+      bbox.parent=this.myBoxes[0].$entity.parent
       var dimensions=new BABYLON.Vector3(this.x, this.y, this.z)
       var translation=dimensions.multiplyByFloats(0.5,0.5,0.5)
       translation.addInPlace({x:-0.5, y:-0.5, z:-0.5})
@@ -104,29 +79,54 @@ export default {
         }
       }
 
-      this.theShapeMesh=theCSG.toMesh({scene: this.myBoxes[0].getScene()})
-      this.theShapeMesh.parent=this.myBoxes[0].parent
+      this.theShapeMesh=theCSG.toMesh({scene: this.myBoxes[0].$entity.getScene()})
+      this.theShapeMesh.parent=this.myBoxes[0].$entity.parent
 
       this.myBoxes.forEach(box => {
-        box.isVisible=false;
-        box.scaling=new BABYLON.Vector3(0.98,0.98,0.98)
-        box.parent=this.theShapeMesh;
-        box.checkCollisions=true;
+        box.$entity.isVisible=false;
+        box.$entity.scaling=new BABYLON.Vector3(0.98,0.98,0.98)
+        box.$entity.parent=this.theShapeMesh;
+        box.$entity.checkCollisions=true;
       })
       // give a material
-      var myMaterial = new BABYLON.StandardMaterial("myMaterial", this.myBoxes[0].getScene());
-      myMaterial.diffuseTexture=new BABYLON.Texture("materials/Wood11_col.jpg", this.myBoxes[0].getScene())
+      var myMaterial = new BABYLON.StandardMaterial("myMaterial", this.myBoxes[0].$entity.getScene());
+      myMaterial.diffuseTexture=new BABYLON.Texture("materials/Wood11_col.jpg", this.myBoxes[0].$entity.getScene())
       myMaterial.diffuseTexture.uScale=0.2
       myMaterial.diffuseTexture.vScale=0.2
-      myMaterial.bumpTexture=new BABYLON.Texture("materials/Wood11_nrm.jpg", this.myBoxes[0].getScene())
+      myMaterial.bumpTexture=new BABYLON.Texture("materials/Wood11_nrm.jpg", this.myBoxes[0].$entity.getScene())
       myMaterial.bumpTexture.uScale=0.2
       myMaterial.bumpTexture.vScale=0.2
-      myMaterial.specularTexture=new BABYLON.Texture("materials/Wood11_rgh.jpg", this.myBoxes[0].getScene())
+      myMaterial.specularTexture=new BABYLON.Texture("materials/Wood11_rgh.jpg", this.myBoxes[0].$entity.getScene())
       myMaterial.specularTexture.uScale=0.2
       myMaterial.specularTexture.vScale=0.2
 
       this.theShapeMesh.material = myMaterial;
-*/    
+    }
+  },
+  computed: {
+    stateString() {
+      return this.entity._text[0].replace(/\d+/g,""); //remove color code
+    },
+    voxelPositions() {
+      var voxArr = new Array();
+      var theId = 0
+      for (let dx=0; dx < this.x; dx++) {
+        for (let dy=0; dy < this.y; dy++) {
+          for (let dz=0; dz < this.z; dz++) {
+            if (this.isVisible(dx, dy, dz)) { voxArr.push({ position: [dx, dy, dz], id:theId }); theId++ }
+          }
+        }
+      }
+      return voxArr
+    },
+    x() { return this.entity._attributes.x},
+    y() { return this.entity._attributes.y},
+    z() { return this.entity._attributes.z},
+    name() { return this.entity._attributes.name},
+    numOfBoxes() { return this.myBoxes.length}
+  },
+  watch: {
+    myBoxes() {
     }
   }
 }
